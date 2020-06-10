6ac4f6f6d206c283611a2f41fab675c0a8fe23bd
nirajkheria <52256838+nirajkheria@users.noreply.github.com>
2020-06-09 22:32:41 -0500

    register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
+  declarations: [AppComponent, ValueComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
