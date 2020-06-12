822b6dfc3b8117b4d21889a0d5106c3731c08ebc
nirajkheria <52256838+nirajkheria@users.noreply.github.com>
2020-06-12 18:56:48 -0500

    fetch list of users , show in cards with animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,22 @@ import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MemberListComponent } from './member-list/member-list.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from "../app/members/member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, ListsComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberCardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
