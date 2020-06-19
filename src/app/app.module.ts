38548bb9e4ecd9b010cc213ccc3cadbd014e54f9
nirajkheria <52256838+nirajkheria@users.noreply.github.com>
2020-06-19 15:51:42 -0500

    reactive form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { TabsModule } from "ngx-bootstrap/tabs";
 import { JwtModule } from "@auth0/angular-jwt";
@@ -50,6 +50,7 @@ export function tokenGetter() {
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
     NgxGalleryModule,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { AuthService } from "../_services/auth.service";
 import { AlertifyService } from "../_services/alertify.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -10,23 +11,45 @@ import { AlertifyService } from "../_services/alertify.service";
 export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() registerCancelled = new EventEmitter();
+  registerForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     private alertifyService: AlertifyService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.registerForm = new FormGroup(
+      {
+        username: new FormControl("", Validators.required),
+        password: new FormControl("", [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(8),
+        ]),
+        confirmPassword: new FormControl("", Validators.required),
+      },
+      this.passwordMatchValidator
+    );
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertifyService.success("Registration Successful");
-      },
-      (error) => {
-        this.alertifyService.error(error);
-      }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertifyService.success("Registration Successful");
+    //   },
+    //   (error) => {
+    //     this.alertifyService.error(error);
+    //   }
+    // );
+
+    console.log(this.registerForm.value);
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get("password").value === g.get("confirmPassword").value
+      ? null
+      : { mismatch: true };
   }
 
   cancel() {
